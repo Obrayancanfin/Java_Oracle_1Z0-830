8c6a6f45846177c886252eaf32c77a350b67e95f
Obrayancanfin <canfinob@gmail.com>
2024-12-20 10:04:40 +0100

    Exercice 20,21,22 about VirtualThread

--- Projet/VirtuelThread/Exo_22/Main.java
+++ Projet/VirtuelThread/Exo_22/Main.java
@@ -1,7 +1,22 @@
 package Exo_22;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
+        CompletableFuture<Void> step1 = CompletableFuture.runAsync(()->{
+            System.out.println("Étape 1 : Lecture des données");},executor);
+
+        CompletableFuture<Void> step2 = step1.thenRunAsync(()->{
+            System.out.println("Étape 2 : Traitment des données");},executor);
+
+        CompletableFuture<Void> step3 = step2.thenRunAsync(()->{
+            System.out.println("Étape 3 : Lecture des données");},executor);
+
+        step3.join();
     }
 }
